397c74b628e1f6d38165d908f6e62551529657dd
Rehmat Ali <itsrehmet@gmail.com>
2025-12-16 00:14:17 +0500

    Initial setup: Vite + React + Tailwind

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+ import React from 'react'
+
+ 
+const App = () => {
+  return (
+    <div>
+      {/* 1. Layout folder: Navbar, Footer, */}
+      {/* Example: <Navbar /> and <Footer /> */}
+      
+      {/* 2. Pages: Full screens like Home,.... .... */}
+      {/* Example: <Home />  */}
+
+      {/* 3. Components: Small reusable pieces like Button, Card, Modal */}
+      {/* Example: <Button /> or <Card /> */}
+
+      
+      <h1 className='text-red-400 text-2xl font-bold'>
+        Start Your Feature
+      </h1>
+
+       
+    </div>
+  )
+}
+
+export default App
